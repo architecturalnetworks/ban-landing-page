6b44a69f2c2138f844c553aabc89ba6212b7890b
Daniel <danrocha@gmail.com>
2020-06-03 12:43:19 +0200

    add option 4

--- pages/index.vue
+++ pages/index.vue
@@ -5,21 +5,26 @@
     </section>
     <div class="h-12" />
     <section class="px-8">
-      <ul class="flex flex-col space-y-4">
-        <li>
+      <ul class="flex flex-col justify-center space-y-4">
+        <li class="text-center">
           <nuxt-link to="/option01" class="font-bold red">Option 01</nuxt-link
           ><br />
           Full logo, form CTA
         </li>
-        <li>
+        <li class="text-center">
           <nuxt-link to="/option02" class="font-bold red">Option 02</nuxt-link
           ><br />
           Circle logo, form CTA
         </li>
-        <li>
+        <li class="text-center">
           <nuxt-link to="/option03" class="font-bold red">Option 03</nuxt-link
           ><br />
           Circle logo, button CTA
+        </li>
+        <li class="text-center">
+          <nuxt-link to="/option04" class="font-bold red">Option 04</nuxt-link
+          ><br />
+          ASCII figures
         </li>
       </ul>
     </section>
