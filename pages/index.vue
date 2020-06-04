7911d88830e114a784d06a246126d31cb8f52e96
Daniel <danrocha@gmail.com>
2020-06-04 14:37:07 +0200

    move home hero to own component

--- pages/index.vue
+++ pages/index.vue
@@ -8,46 +8,7 @@
         </div>
         <div class="h-8 md:hidden" />
         <!-- HERO -->
-        <section
-          class="flex justify-around max-w-3xl mx-auto text-lg text-red-600 md:justify-between font-logo sm:text-xl"
-        >
-          <div class="hidden text-black md:block">
-            <p>|</p>
-            <p><span class="text-red-600">&middot; b</span>erlin</p>
-            <p>| <span class="text-red-600">a</span>rchitecture</p>
-            <p>| <span class="text-red-600">n</span>etwork</p>
-          </div>
-          <div>
-            <p>&nbsp;</p>
-            <p>&nbsp;[]&nbsp;</p>
-            <p>[++]</p>
-            <p>/&nbsp;&nbsp;\</p>
-          </div>
-          <div>
-            <p>&nbsp;</p>
-            <p>\[]/</p>
-            <p>&nbsp;--&nbsp;</p>
-            <p>/&nbsp;&nbsp;\</p>
-          </div>
-          <div>
-            <p>&nbsp;</p>
-            <p>&nbsp;&nbsp;[]&nbsp;</p>
-            <p>&nbsp;/--&nbsp;</p>
-            <p>[*]&nbsp;\</p>
-          </div>
-          <div>
-            <p>&nbsp;</p>
-            <p>&nbsp;[]&nbsp;</p>
-            <p>&lt;\/&gt;</p>
-            <p>/&nbsp;&nbsp;\</p>
-          </div>
-          <div>
-            <p>&nbsp;</p>
-            <p>&nbsp;[]&nbsp;</p>
-            <p>&lt;--&gt;</p>
-            <p>/&nbsp;&nbsp;\</p>
-          </div>
-        </section>
+        <home-hero />
       </header>
 
       <div class="h-12 md:h-24" />
