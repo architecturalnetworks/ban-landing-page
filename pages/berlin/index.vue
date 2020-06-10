325cb1ce5dc3a3dbe87983ef0847e2d3726c7f39
Daniel <danrocha@gmail.com>
2020-06-10 21:06:09 +0200

    fix style

--- pages/berlin/index.vue
+++ pages/berlin/index.vue
@@ -45,7 +45,8 @@
           <!-- <div class="w-12 mb-1 border-t-2 border-red-600" /> -->
           <div class="flex justify-between">
             <h2 class="font-bold">
-              Events <span class="text-red-600 font-base font-logo">next</span>
+              Events
+              <span class="font-normal text-red-600 font-logo">next</span>
             </h2>
             <p class="font-logo">[O]</p>
           </div>
@@ -100,7 +101,7 @@
         <!-- <div class="w-12 mb-1 border-t-2 border-white" /> -->
         <h2 class="font-bold">
           About
-          <span class="text-red-600 font-base font-logo">&middot;ban</span>
+          <span class="font-normal text-red-600 font-logo">&middot;ban</span>
         </h2>
         <div class="h-4" />
         <div>
