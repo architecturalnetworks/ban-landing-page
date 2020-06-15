921ae5d933eeae9c0fb5b5123b07f44a9b30218d
Daniel <danrocha@gmail.com>
2020-06-15 09:23:34 +0200

    implement logo component

---
+++ components/LogoFullText.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="font-logo">
+    <p>|</p>
+    <p><span class="text-red-600">&middot; b</span>erlin</p>
+    <p>| <span class="text-red-600">a</span>rchitectural</p>
+    <p>| <span class="text-red-600">n</span>etwork</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
