f6098c37489e69e5d5ba60131ecf5d2c3700c62d
Daniel <danrocha@gmail.com>
2020-06-15 09:24:17 +0200

    create events page

---
+++ pages/berlin/events/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-4xl font-bold">
+      Events<span class="font-normal text-red-600 font-logo"> next</span>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'pages',
+}
+</script>
+
+<style lang="scss" scoped></style>
