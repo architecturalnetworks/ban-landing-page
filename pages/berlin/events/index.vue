1f1d4fa01bed8849463f41141c0c1bdfa9a23ea7
Daniel <danrocha@gmail.com>
2020-06-17 07:30:58 +0200

    implement events page

--- pages/berlin/events/index.vue
+++ pages/berlin/events/index.vue
@@ -3,12 +3,43 @@
     <h1 class="text-4xl font-bold">
       Events<span class="font-normal text-red-600 font-logo"> next</span>
     </h1>
+    <div class="h-8" />
+    <event-list-item-next :event="futureEvents[0]" />
+    <event-list v-if="futureEvents.length > 1" :events="futureEvents" />
+    <div class="h-10" />
+    <h2 class="text-2xl font-bold">
+      Events<span class="font-normal text-red-600 font-logo"> past</span>
+    </h2>
+    <div class="h-8" />
+    <event-list :events="pastEvents" />
+    <div class="h-8" />
   </div>
 </template>
 
 <script>
 export default {
   layout: 'pages',
+  async asyncData({ $content }) {
+    const today = new Date()
+    const yesterday = new Date(today)
+    yesterday.setDate(yesterday.getDate() - 1)
+    const futureEvents = await $content('events')
+      .where({
+        publish: true,
+        date: { $gt: yesterday },
+      })
+      .fetch()
+    const pastEvents = await $content('events')
+      .where({
+        publish: true,
+        date: { $lt: today },
+      })
+      .fetch()
+    return {
+      futureEvents,
+      pastEvents,
+    }
+  },
 }
 </script>
 
