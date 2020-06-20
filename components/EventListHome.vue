076b1118de0b21847f268f7ae0d3389b1bf11740
Daniel <danrocha@gmail.com>
2020-06-20 11:42:05 +0200

    fix spacing

--- components/EventListHome.vue
+++ components/EventListHome.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <event-list-item-next :event="events[0]" />
-    <ul v-if="events.length > 1" class="space-y-8">
-      <li v-for="event in events.slice(1, -1)" :key="event.slug">
+    <div class="h-4" />
+    <ul v-if="events.length > 1" class="space-y-4">
+      <li v-for="event in events.slice(1)" :key="event.slug">
         <event-list-item :event="event" />
       </li>
     </ul>
