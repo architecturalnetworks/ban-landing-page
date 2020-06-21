86b85ccc7b579889683267db32e360031f09da96
Daniel <danrocha@gmail.com>
2020-06-21 09:07:18 +0200

    add icons

---
+++ components/icons/IconVideo.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="inline-block mb-1"
+  >
+    <polygon points="23 7 16 12 23 17 23 7"></polygon>
+    <rect x="1" y="5" width="15" height="14" rx="2" ry="2"></rect>
+  </svg>
+  <!-- <svg
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    stroke="black"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="miter"
+    fill="none"
+    color="black"
+  >
+    <path stroke-linecap="round" d="M16 10.087l5-1.578v7.997l-4.998-1.578" />
+    <path d="M16 7H3v11h13z" />
+  </svg> -->
+</template>
