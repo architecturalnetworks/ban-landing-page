950f5fc291d9e3274e343d98c7dd54805976669a
Daniel da Rocha <danrocha@gmail.com>
2020-07-26 11:24:41 +0200

    fix navbar width

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,19 +1,21 @@
 <template>
-  <nav class="flex items-center justify-between">
-    <nuxt-link to="/berlin" tag="button" class="text-left focus:outline-none"
-      ><logo-full-text :class="size === 'large' ? 'text-lg' : 'text-base'"
-    /></nuxt-link>
+  <div>
+    <nav class="flex items-center justify-between w-full">
+      <nuxt-link to="/berlin" tag="button" class="text-left focus:outline-none"
+        ><logo-full-text :class="size === 'large' ? 'text-lg' : 'text-base'"
+      /></nuxt-link>
 
-    <div class="font-logo">
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <p class="font-sans font-bold">
-        <nuxt-link to="/berlin/events">Events</nuxt-link>
-      </p>
+      <div class="font-logo">
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p class="font-sans font-bold">
+          <nuxt-link to="/berlin/events">Events</nuxt-link>
+        </p>
 
-      <p>&nbsp;</p>
-    </div>
-  </nav>
+        <p>&nbsp;</p>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
